aded911d62e462c8ecb82d32c93e8bf8bc421de6
Chase C. Miller <chasecmiller@gmail.com>
2017-07-22 21:23:49 -0600

    Initial Push

---
+++ Crumbls/Theme/Admin.php
@@ -0,0 +1,14 @@
+<?php
+namespace Crumbls\Theme;
+/**
+ * A terribly useful class
+ *
+ * See: http://knpuniversity.com/screencast/question-answer-day
+ */
+class Admin extends Common
+{
+    public function __construct() {
+        parent::__construct();
+    }
+
+}
---
+++ Crumbls/Theme/Common.php
@@ -0,0 +1,13 @@
+<?php
+namespace Crumbls\Theme;
+/**
+ * A terribly useful class
+ *
+ * See: http://knpuniversity.com/screencast/question-answer-day
+ */
+class Common
+{
+    public function __construct() {
+    }
+
+}
